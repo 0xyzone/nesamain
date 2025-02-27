f025307c298be9abe5dbcdff098891352d74ae20
0xyzone <58982560+0xyzone@users.noreply.github.com>
2025-02-27 16:22:19 +0545

    Update fyftnec.blade.php

--- resources/views/fyftnec.blade.php
+++ resources/views/fyftnec.blade.php
@@ -66,7 +66,7 @@
                     <h1 class="text-base md:text-xl lg:text-3xl 2xl:text-5xl uppercase font-bold text-center">
                         Are You Ready to Compete?
                     </h1>
-                    <a href="https://meroticket.com" class="button-styled">Register Now</a>
+                    <a href="https://meroticket.com/tournament-registration" class="button-styled">Register Now</a>
                 </div>
             </div>
         </div>
